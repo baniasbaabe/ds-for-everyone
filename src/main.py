ac5b26a9a55295cfd7e006120d5dd4b8b47084b7
baniasbaabe <banias@hotmail.de>
2022-04-17 14:43:45 +0200

    Add configuration app

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import streamlit as st
 
-from apps import data_upload_app, multipage
+from apps import data_upload_app, configurations_app, multipage
 
 if __name__ == "__main__":
     st.set_option("deprecation.showfileUploaderEncoding", False)
@@ -14,5 +14,6 @@ if __name__ == "__main__":
 
     app.add_page("Upload Data", data_upload_app.app)
 
-    app.run()
+    app.add_page("Change Data Types", configurations_app.app)
 
+    app.run()
