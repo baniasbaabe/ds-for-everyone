7927308d799061b95a1239646fcf73d96357b8e5
baniasbaabe <banias@hotmail.de>
2022-04-18 15:33:22 +0200

    Refactor code

--- src/apps/data_upload_app.py
+++ src/apps/data_upload_app.py
@@ -4,6 +4,8 @@ from typing import Any, List
 import pandas as pd
 import streamlit as st
 
+from .utils.utils import get_file_path, get_file_type, read_file
+
 
 def app():
     df = None
@@ -18,22 +20,5 @@ def app():
     file_type = get_file_type(uploaded_file)
     df = read_file(uploaded_file, file_type)
 
-    path = get_file_path(["..", "data", "uploaded_file.csv"])
+    path = get_file_path(["..", "..", "data", "uploaded_file.csv"])
     df.to_csv(path, index = False)
-
-def read_file(path: Any, file_type: str) -> pd.DataFrame:
-    if file_type == "csv":
-        df = pd.read_csv(path)
-    elif file_type == "xlsx":
-        df = pd.read_xlsx(path)
-    return df
-
-def get_file_type(file: str) -> str:
-    try:
-        extension = file.name.split(".")[-1]
-    except AttributeError:
-        extension = file.split(".")[-1]
-    return extension
-
-def get_file_path(directory: List) -> str:
-    return os.path.abspath(os.path.join(os.path.dirname( __file__ ), *directory))
