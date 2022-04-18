ff1ba2a95a0988898ee0f389ab17b31a7bb92b07
baniasbaabe <banias@hotmail.de>
2022-04-18 15:34:06 +0200

    Create folder for util functions

---
+++ src/apps/utils/utils.py
@@ -0,0 +1,20 @@
+import os
+from typing import Any, List
+import pandas as pd
+
+def get_file_path(directory: List) -> str:
+    return os.path.abspath(os.path.join(os.path.dirname( __file__ ), *directory))
+
+def get_file_type(file: str) -> str:
+    try:
+        extension = file.name.split(".")[-1]
+    except AttributeError:
+        extension = file.split(".")[-1]
+    return extension
+
+def read_file(path: Any, file_type: str) -> pd.DataFrame:
+    if file_type == "csv":
+        df = pd.read_csv(path)
+    elif file_type == "xlsx":
+        df = pd.read_xlsx(path)
+    return df
