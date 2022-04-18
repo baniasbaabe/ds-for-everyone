f66bdb3c3a41eb1180bd07a2f8c695f5cd85f27e
baniasbaabe <banias@hotmail.de>
2022-04-18 15:49:35 +0200

    Add eda app as new page

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import streamlit as st
 
-from apps import data_upload_app, configurations_app, multipage
+from apps import data_upload_app, configurations_app, eda_app, multipage
 
 if __name__ == "__main__":
     st.set_option("deprecation.showfileUploaderEncoding", False)
@@ -15,5 +15,7 @@ if __name__ == "__main__":
     app.add_page("Upload Data", data_upload_app.app)
 
     app.add_page("Change Data Types", configurations_app.app)
+
+    app.add_page("EDA", eda_app.app)
 
     app.run()
