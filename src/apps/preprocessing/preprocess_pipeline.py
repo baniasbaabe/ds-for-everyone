6301078c8e90425d6df4856d45dcc8ee350aa010
baniasbaabe <banias@hotmail.de>
2022-04-28 19:39:30 +0200

    Add generic base pipeline for both classification and regression

---
+++ src/apps/preprocessing/preprocess_pipeline.py
@@ -0,0 +1,36 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import KNNImputer, SimpleImputer
+from category_encoders.target_encoder import TargetEncoder
+
+def cat_pipeline():
+    pipeline = Pipeline([
+        ("cat_impute", SimpleImputer(strategy = "constant", fill_value = "Missing")),
+        ("encoder", TargetEncoder())
+    ])
+
+    return pipeline
+
+def num_pipeline():
+    pipeline = Pipeline([
+        ("num_impute", KNNImputer(n_neighbors = 5)),
+    ])
+
+    return pipeline
+
+def column_transformer_pipeline(cat_cols, num_cols):
+    preprocessor = ColumnTransformer([
+        ("cat", cat_pipeline(), cat_cols),
+        ("num", num_pipeline(), num_cols)
+    ])
+
+    return preprocessor
+
+def full_pipeline(cat_cols, num_cols, model):
+    pipeline = Pipeline([
+        ("preprocessor", column_transformer_pipeline(cat_cols, num_cols)),
+        ("model", model)
+    ])
+    
+    return pipeline
