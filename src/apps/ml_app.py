1d87dc5a50acfa08b9e573d4a8d72dc60cfa9f38
baniasbaabe <banias@hotmail.de>
2022-04-29 19:19:29 +0200

    Add visualizer for classification

--- src/apps/ml_app.py
+++ src/apps/ml_app.py
@@ -2,11 +2,24 @@ import os
 import optuna
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
+import xgboost as xgb
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from yellowbrick.classifier.classification_report import classification_report
 from .utils.utils import get_file_path
 from .preprocessing import feature_drop, preprocess_pipeline
 from .optimizer import optimize_classification, optimize_regression
+
+optuna.logging.set_verbosity(optuna.logging.CRITICAL)
+
+models_classification = {
+    "XGBoost": xgb.XGBClassifier,
+    "RandomForest": RandomForestClassifier,
+    "AdaBoost": AdaBoostClassifier,
+}
 
 optimizer_collection = {
     "Classification": optimize_classification.objective,
@@ -16,6 +29,8 @@ optimizer_collection = {
 def app():
     if "uploaded_file.csv" not in os.listdir(get_file_path(["..", "..", "data"])):
         st.write("You have to upload your data in 'Upload data' section")
+    elif pd.read_csv(get_file_path(["..", "..", "data", "uploaded_file.csv"])).shape[1] < 2:
+        st.write("Your file doesn't contain 2 columns or more. Upload a new appropiate file.")
     else:
         data = pd.read_csv(get_file_path(["..", "..", "data", "uploaded_file.csv"]))
 
@@ -51,12 +66,27 @@ def app():
         if st.button("Run Machine Learning Algorithms"):
             y = data.loc[:, target].copy()
             X = data.loc[:, data.columns != target].copy()
-
             le = LabelEncoder()
             y = le.fit_transform(y)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+            cat_cols = X.select_dtypes(include = ["object", "category"]).columns.to_list()
+            num_cols = X.select_dtypes(include = np.number).columns.to_list()
 
             study = optuna.create_study(direction = "minimize")
-            func = lambda trial: optimizer(trial, X, y, preprocess_pipeline.full_pipeline)
-            study.optimize(func, n_trials = 3, show_progress_bar = False)
+            func = lambda trial: optimizer(trial, X_train, y_train, preprocess_pipeline.full_pipeline)
+            study.optimize(func, n_trials = 3, show_progress_bar = True)
+            st.write(study.best_params)
+            model = models_classification[study.best_params["classifier_name"]]
+            # study.best_params = study.best_params.pop("classifier_name")
+            model = model(**{i:study.best_params[i] for i in study.best_params if i != "classifier_name"})
 
-            print(study.best_params)
+            pipeline = preprocess_pipeline.full_pipeline(cat_cols, num_cols, model)
+
+            pipeline.fit(X_train, y_train)
+        
+            fig, ax = plt.subplots(figsize=(10,10))
+                # visualizer = ClassificationReport(model, classes=le.classes_, support=True)
+                # visualizer.fit(X_train, y_train)        # Fit the visualizer and the model
+            classification_report(pipeline, X_train, y_train, ax = ax)
+            st.pyplot(fig)
