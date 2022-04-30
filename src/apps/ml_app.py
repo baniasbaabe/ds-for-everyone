3187f9c4e9febcbfc37d0ae9b6d07bbe45c22307
baniasbaabe <banias@hotmail.de>
2022-04-30 11:56:43 +0200

    Add regression plots

--- src/apps/ml_app.py
+++ src/apps/ml_app.py
@@ -4,12 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import optuna
 import pandas as pd
+import shap
 import streamlit as st
 import xgboost as xgb
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, AdaBoostRegressor, RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from yellowbrick.classifier.classification_report import classification_report
+from yellowbrick.classifier.confusion_matrix import confusion_matrix
+from yellowbrick.classifier.rocauc import roc_auc
+from yellowbrick.regressor.prediction_error import prediction_error
+from yellowbrick.regressor.residuals import residuals_plot
 
 from .optimizer import optimize_classification, optimize_regression
 from .preprocessing import feature_drop, preprocess_pipeline
@@ -18,14 +23,20 @@ from .utils.utils import get_file_path
 optuna.logging.set_verbosity(optuna.logging.CRITICAL)
 
 models_classification = {
-    "XGBoost": xgb.XGBClassifier,
-    "RandomForest": RandomForestClassifier,
-    "AdaBoost": AdaBoostClassifier,
+    "XGBoost": xgb.XGBRegressor,
+    "RandomForest": RandomForestRegressor,
+    "AdaBoost": AdaBoostRegressor,
+}
+
+models_regression = {
+    "XGBoost": xgb.XGBRegressor,
+    "RandomForest": RandomForestRegressor,
+    "AdaBoost": AdaBoostRegressor,
 }
 
 optimizer_collection = {
     "Classification": optimize_classification.objective,
-    # "Regression": optimize_regression.objective,
+    "Regression": optimize_regression.objective,
 }
 
 
@@ -90,15 +101,18 @@ def app():
             func = lambda trial: optimizer(
                 trial, X_train, y_train, preprocess_pipeline.full_pipeline
             )
-            study.optimize(func, n_trials=3, show_progress_bar=True)
+            study.optimize(func, n_trials=4, show_progress_bar=True)
             st.write(study.best_params)
-            model = models_classification[study.best_params["classifier_name"]]
-            # study.best_params = study.best_params.pop("classifier_name")
+            if type_of_ml_problem == "Classification":
+                model = models_classification[study.best_params["model_name"]]
+            else:
+                model = models_regression[study.best_params["model_name"]]
+
             model = model(
                 **{
                     i: study.best_params[i]
                     for i in study.best_params
-                    if i != "classifier_name"
+                    if i != "model_name"
                 }
             )
 
@@ -106,9 +120,48 @@ def app():
 
             pipeline.fit(X_train, y_train)
 
-            if st.button("Plot Classification Report"):
+            if type_of_ml_problem == "Classification":
+                st.write("Classification Report")
                 fig, ax = plt.subplots(figsize=(10, 10))
                 classification_report(
-                    pipeline, X_train, y_train, ax=ax, classes=le.classes_
+                    pipeline, X_train, y_train, X_test, y_test, ax=ax, classes=le.classes_
                 )
                 st.pyplot(fig)
+
+                st.write("Confusion Matrix")
+                fig, ax = plt.subplots(figsize=(10, 10))
+                confusion_matrix(
+                    pipeline, X_train, y_train, X_test, y_test, ax=ax, classes=le.classes_
+                )
+                st.pyplot(fig)
+
+                st.write("ROC")
+                fig, ax = plt.subplots(figsize=(10, 10))
+                is_binary = True if pd.Series(y).nunique() == 2 else False
+                roc_auc(
+                    pipeline,
+                    X_train,
+                    y_train,
+                    X_test,
+                    y_test,
+                    ax=ax,
+                    classes=le.classes_,
+                    binary=is_binary,
+                )
+                st.pyplot(fig)
+            else:
+                st.write("Prediction Error")
+                fig, ax = plt.subplots(figsize=(10, 10))
+                prediction_error(pipeline, X_train, y_train, X_test, y_test, ax = ax)
+                st.pyplot(fig)
+
+                st.write("Residuals Plot")
+                fig, ax = plt.subplots(figsize=(10, 10))
+                residuals_plot(pipeline, X_train, y_train, X_test, y_test, ax = ax)
+                st.pyplot(fig)
+            # st.write("Interpret Model with SHAP")
+            # fig = plt.figure()
+            # observations = pipeline["preprocessor"].transform(X_test)
+            # shap_values = shap.KernelExplainer(pipeline["model"].predict_proba, observations).shap_values(observations)
+            # shap.summary_plot(shap_values, observations, plot_type = "bar")
+            # st.pyplot(fig)
